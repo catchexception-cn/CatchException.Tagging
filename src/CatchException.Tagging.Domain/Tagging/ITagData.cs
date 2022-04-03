eaa96c8b5cbd51956ef4c0eb1be44051bd84153d
Chi Zhang <zliming@outlook.com>
2022-04-03 20:30:06 +0800

    define ITagData

---
+++ src/CatchException.Tagging.Domain/Tagging/ITagData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+using Volo.Abp.Domain.Entities;
+
+namespace CatchException.Tagging.Tagging;
+
+public interface ITagData : IAggregateRoot<Guid>
+{
+    string Name { get; }
+
+    string Description { get; }
+}
